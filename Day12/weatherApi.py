a9ac5e4c2bc6587e86390610c27916a1aa0827ca
Hrushikesh Shukla <72937372+Rishicr73@users.noreply.github.com>
2021-10-04 21:38:48 +0530

    Update weatherApi.py

--- Day12/weatherApi.py
+++ Day12/weatherApi.py
@@ -3,11 +3,11 @@ import json
 
 city_name = input('City Name   : ').lower()
 
-api_key = 'your api key'
+api_key = 'your api key' #Add your key here then only you can fetched the weather info of city.
 
 url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units={'metric'}"
 
-response = requests.get(url)
+response = requests.get(url)  #to request data from the server.
 
 if response.status_code == 200:        
     response = response.json()
